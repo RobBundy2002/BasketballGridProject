def4c8a621d8002013cd34f94c60a0679e73e498
Patrick Williamson <upm8pb@virginia.edu>
2023-09-28 13:52:29 -0400

    python scripts

---
+++ python_scripts/get_team_list.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import json
+from time import sleep
+import get_proxy
+'''
+This program gets a all teams from a conference and saves them into a json file year by year.
+Ask me for the password
+'''
+# writes to a file named "confrence_year.txt"
+n = len(sys.argv)
+if n == 1:
+    print("<confrence abbr>" "proxy password")
+    sys.exit()
+confrence = sys.argv[1].lower()
+global password
+password = sys.argv[2]
+def teams_getter(url,year,data):
+    proxies = get_proxy.get_proxies(password)
+    response=requests.get(url,proxies=proxies)
+    if response.status_code == 200:
+        html = response.text
+    else:
+        print(response.status_code)
+        return data
+    data[year] = []
+    soup = BeautifulSoup(html,'html.parser')
+    table_id = "standings"
+    table = soup.find('table',{'id':table_id})
+    for row in table.find_all('tr'):
+        for cell in row.find_all('td',{'data-stat':'school_name'}):
+            data[year].append(cell.text)
+    year -= 1
+    print(f"Currently on year {year}")
+    return teams_getter(f"https://www.sports-reference.com/cbb/conferences/{confrence}/men/{year}.html",year,data)
+
+year = 2023
+url = f"https://www.sports-reference.com/cbb/conferences/{confrence}/men/{year}.html"
+data = {}
+teams_getter(url,year,data)
+with open(f"{confrence}.json","w") as json_file:
+    json.dump(data,json_file)
