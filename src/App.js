ae9f60a24baab0b86642b8dd2ab4c79acd926d22
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-10-29 01:10:25 -0400

    Season Averages Categories Finished

--- src/App.js
+++ src/App.js
@@ -25,15 +25,18 @@ function App() {
     "Missouri", "NC State", "Nebraska", "Northwestern", "Notre Dame", "Ohio State", "Oklahoma State", "Oklahoma", "Ole Miss",
     "Oregon State", "Oregon", "Penn State", "Pitt", "Purdue", "Rutgers", "South Carolina", "Stanford", "Syracuse", "TCU",
     "Tennessee", "Texas A&M", "Texas Tech", "Texas", "UCLA", "UNC", "USC", "Utah", "Vanderbilt", "Virginia Tech", "Virginia", "Wake Forest", 
-    "Washington State", "Washington", "West Virginia", "Wisconsin", "ACC"]; 
+    "Washington State", "Washington", "West Virginia", "Wisconsin", "ACC", "Big 12", "Big Ten", "Pac-12", "SEC"]; 
 
   const categoriesList = ["+ Points Per Game (Season)", "+ Rebounds Per Game (Season)", "+ Assists Per Game (Season)",
-    "+ Blocks Per Game (Season)", "+ Steals Per Game (Season)"];
+    "+ Blocks Per Game (Season)", "+ Steals Per Game (Season)", "+ Minutes Per Game (Season)", "+ FG Made Per Game (Season)",
+    "%+ FG Percentage (Season)", "+ 3PT Made Per Game (Season)", "%+ 3PT Percentage (Season)", "+ FT Made Per Game (Season)",
+    "%+ FT Percentage (Season)", "+ Turnovers Per Game (Season)", "+ Fouls Per Game (Season)"];
 
   const gridAnswers = [];
 
-  // TODO: ADD BIG12, BIG10, SEC, PAC12 LOGOS 500 X 500px
   // TODO: ADD REST OF CATEGORIES TO GENERATE GRID FUNCTION
+  // TODO: MAKE ONE USEEFFECT METHOD
+  // TODO: MERGE ROB'S CODE TO THIS BRANCH
 
   useEffect(() => {
     let grid = generateGrid();
@@ -162,6 +165,204 @@ function App() {
             const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
             const hasSTLS = player.season_averages.steals_per_game;
             return hasSchool && hasSTLS >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "+ Minutes Per Game (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 25 AND 40
+        const random = Math.floor(Math.random() * (16)) + 25;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasMINS = player.season_averages.minutes_per_game;
+            return hasSchool && hasMINS >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "+ FG Made Per Game (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 4 AND 8
+        const random = Math.floor(Math.random() * (5)) + 4;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasFGS = player.season_averages.fg_per_game;
+            return hasSchool && hasFGS >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "%+ FG Percentage (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 45 AND 60
+        const random = Math.floor(Math.random() * (16)) + 45;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasFGP = player.season_averages.fg_percentage;
+            return hasSchool && hasFGP*100 >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "+ 3PT Made Per Game (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 2 AND 5
+        const random = Math.floor(Math.random() * (4)) + 2;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const has3FG = player.season_averages.three_point_per_game;
+            return hasSchool && has3FG >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "%+ 3PT Percentage (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 35 AND 45
+        const random = Math.floor(Math.random() * (11)) + 35;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const has3FGP = player.season_averages.three_point_percentage;
+            return hasSchool && has3FGP*100 >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "+ FT Made Per Game (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 4 AND 10
+        const random = Math.floor(Math.random() * (7)) + 4;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasFTM = player.season_averages.ft_per_game;
+            return hasSchool && hasFTM >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "%+ FT Percentage (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 75 AND 90
+        const random = Math.floor(Math.random() * (16)) + 75;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasFTP = player.season_averages.ft_percentage;
+            return hasSchool && hasFTP*100 >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "+ Turnovers Per Game (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 1 AND 3
+        const random = Math.floor(Math.random() * (3)) + 1;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasTOV = player.season_averages.turnovers_per_game;
+            return hasSchool && hasTOV >= random
+          })
+          if (playerOptions.length < 3) {
+            //INVALID GRID
+            return false;
+          }
+          else {
+            //ADD CORRECT PLAYERS TO ARRAY
+            gridAnswers[i][j] = playerOptions;
+          }
+        }
+      }
+      else if (attribute === "+ Fouls Per Game (Season)"){
+        //GENERATE A RANDOM NUMBER BETWEEN 2 AND 4
+        const random = Math.floor(Math.random() * (3)) + 2;
+        numbers[i] = random;
+        for(let j=0; j<3; j++){
+          let school = schools[j]
+          //FILTER THROUGH JSON DATA
+          const playerOptions = jsonData.filter(player => {
+            const hasSchool = player.teams.includes(school) || player.conferences.includes(school);
+            const hasPF = player.season_averages.fouls_per_game;
+            return hasSchool && hasPF >= random
           })
           if (playerOptions.length < 3) {
             //INVALID GRID
