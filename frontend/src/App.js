67db0c9667de78b78d9a0d1bf2ce86cfea1188c5
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-18 14:46:30 -0500

    Moved current app into frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import HoopGrid from './components/HoopGrid';
+import {StyledTopText} from "./components/styles/StyledTopText";
+import {StyledTopBar} from "./components/styles/StyledTopBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="pop"> <StyledTopBar>
+          <img alt="Basketball Logo" src={process.env.PUBLIC_URL + '/Untitled.ico'} />
+          {/* Content for the top bar */}
+      </StyledTopBar>
+
+      <div className="App">
+          <div className='top-bar'>
+              {/* Use the absolute path with the PUBLIC_URL prefix for the image */}
+              {/*<StyledTopBar>*/}
+              {/*<img alt="Basketball Logo" src={process.env.PUBLIC_URL + '/basketball logo 1.ico'} />*/}
+              {/*/!* Content for the top bar *!/*/}
+              {/*</StyledTopBar>*/}
+              <StyledTopText>
+                  College Hoops Matrix Madness
+              </StyledTopText>
+          </div>
+          <HoopGrid></HoopGrid>
+      </div>
+      </div>
   );
 }
 
-export default App;
+export default App;       
