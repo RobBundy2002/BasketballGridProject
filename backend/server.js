476c3a3922158679794ab79b1bd291a4fe33e189
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-18 17:22:42 -0500

    server.js

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+//const workoutRoutes = require('./routes/workouts')
+
+// EXPRESS APP
+const app = express()
+
+// MIDDLEWARE
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// ROUTES
+//app.use('/api/workouts', workoutRoutes)
+
+// CONNECT TO DATABASE
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // LISTEN FOR REQUESTS
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
