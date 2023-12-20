4b0196801cce5c1883b652c39ec8bef7acfddbd5
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-19 23:50:58 -0500

    test function to run everyday at 2:00am

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const matrixRoutes = require('./routes/matrix')
+const cron = require('node-cron');
+const createMatrix = require('./controllers/matrixController')
 
 // EXPRESS APP
 const app = express()
@@ -21,11 +23,20 @@ app.use('/api/matrix', matrixRoutes)
 // CONNECT TO DATABASE
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
+        // SCHEDULE DAILY TASK
+        const dailyTask = () => {
+            // Place your code here that you want to run daily
+            console.log('Running daily task at', new Date());
+        };
+
+        // Schedule the task to run every day at a specific time (in this example, at 2:00 AM)
+        cron.schedule('0 2 * * *', dailyTask);
+
         // LISTEN FOR REQUESTS
         app.listen(process.env.PORT, () => {
-            console.log('connected to db & listening on port', process.env.PORT)
-        })
+            console.log('Connected to db & listening on port', process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
