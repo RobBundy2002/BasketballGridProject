a19dbf63dd73e039f6cceefdc2ff43552ba65a49
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-20 13:24:33 -0500

    npm install node-fetch

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,37 @@ app.use('/api/matrix', matrixRoutes)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // SCHEDULE DAILY TASK
-        const dailyTask = () => {
+        const dailyTask = async () => {
             // Place your code here that you want to run daily
-            console.log('Running daily task at', new Date());
+            console.log('Running daily task at', new Date())
+            const currentDate = new Date()
+            const tomorrowDate = new Date()
+            tomorrowDate.setDate(currentDate.getDate()+1)
+            const day = tomorrowDate.getDate()
+            const month = tomorrowDate.getMonth() + 1
+            const year = tomorrowDate.getFullYear()
+
+            const matrixData = {month, day, year}
+            const response = await fetch('localhost:4000/api/matrix', {
+                method: 'POST',
+                body: JSON.stringify(matrixData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const json = await response.json()
+            console.log(json)
+            if(!response.ok) {
+                console.log("Didn't work")
+            }
+            if (response.ok) {
+                console.log('New Matrix Added', json)
+            }
+            console.log("Finished")
         };
 
         // Schedule the task to run every day at a specific time (in this example, at 2:00 AM)
-        cron.schedule('0 2 * * *', dailyTask);
+        cron.schedule('20 13 * * *', dailyTask);
 
         // LISTEN FOR REQUESTS
         app.listen(process.env.PORT, () => {
