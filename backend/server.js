064ae180b15d140a22bcda2b89eefd1418346160
gcostello02 <GRANT.COSTELLO.02@gmail.com>
2023-12-20 14:35:01 -0500

    Getting rid of testing logs

--- backend/server.js
+++ backend/server.js
@@ -24,28 +24,20 @@ app.use('/api/matrix', matrixRoutes)
 // CONNECT TO DATABASE
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        // Schedule the createMatrix function to run every day at 11 PM
+        // SCHEDULED FUNCTION AT 11PM
         cron.schedule('0 23 * * *', () => {
             console.log("Running Daily ", new Date())
-            // Mock request and response for testing
             const today = new Date();
             const tomorrow = new Date();
             tomorrow.setDate(today.getDate() + 1)
-            console.log("After Dates")
             const mockRequest = { body: { month: tomorrow.getMonth()+1, day: tomorrow.getDate(), year: tomorrow.getFullYear() } };
-            console.log("After Request")
             const mockResponse = {
                 status: (code) => ({
                     json: (data) => console.log(`Response: ${code}`, data),
                 }),
             };
-            console.log("After Response")
-
-
-            // Call the createMatrix function with the mock request and response
             try {
                 createMatrix(mockRequest, mockResponse);
-                console.log("Finished");
             } catch (error) {
                 console.error("Error in createMatrix:", error);
             }
